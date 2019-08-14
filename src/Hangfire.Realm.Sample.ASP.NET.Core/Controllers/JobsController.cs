1a5d0293c00f3b9c72ab8727e2edca83a699644c
Gregory Buchenberger <buchenberger@gmail.com>
2019-08-14 08:46:49 -0600

    clean up samples

---
+++ src/Hangfire.Realm.Sample.ASP.NET.Core/Controllers/JobsController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hangfire.Realm.Sample.ASP.NET.Core.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class JobsController
+    {
+        // POST api/jobs
+        [HttpPost]
+        [Produces("application/json")]
+        [Route("api/jobs/new")]
+        public void Post([FromBody] string value)
+        {
+            BackgroundJobServerOptions serverOptions = new BackgroundJobServerOptions()
+            {
+                WorkerCount = 1,
+                Queues = new[] { "critical", "default" },
+                ServerTimeout = TimeSpan.FromMinutes(10),
+                HeartbeatInterval = TimeSpan.FromSeconds(10),
+                ServerCheckInterval = TimeSpan.FromSeconds(10),
+                SchedulePollingInterval = TimeSpan.FromSeconds(10),
+                //CountersAggregateInterval = TimeSpan.FromMinutes(1),
+                //ExpirationCheckInterval = TimeSpan.FromMinutes(15),
+
+            };
+            using (new BackgroundJobServer(serverOptions))
+            {
+                for (var i = 0; i < 50; i++)
+                {
+                    var jobNumber = i + 1;
+                    var jobId = BackgroundJob.Enqueue(() =>
+                    Console.WriteLine($"Fire-and-forget job {jobNumber}"));
+                    //Console.WriteLine($"Job {jobNumber} was given Id {jobId} and placed in queue");
+                }
+
+                BackgroundJob.Schedule(() =>
+                Console.WriteLine("Scheduled job"),
+                TimeSpan.FromSeconds(30));
+
+            }
+        }
+    }
+}
---
+++ src/Hangfire.Realm.Sample.ASP.NET.Core/Startup.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using System.IO;
+using Hangfire.Logging.LogProviders;
+using Microsoft.AspNetCore.Http;
+using Realms;
+
+namespace Hangfire.Realm.Sample.ASP.NET.Core
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            RealmJobStorageOptions storageOptions = new RealmJobStorageOptions
+            {
+                RealmConfiguration = new RealmConfiguration(Path.Combine(Directory.GetCurrentDirectory(), "sample.realm"))
+            };
+            
+            services.AddHangfire(config =>
+            {
+                config
+                .UseRealmJobStorage(storageOptions)
+                .UseLogProvider(new ColouredConsoleLogProvider());
+            });
+            services.AddHangfireServer();
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            app.UseHangfireServer();
+            app.UseHangfireDashboard();
+            app.UseStaticFiles();
+            app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+    }
+}
