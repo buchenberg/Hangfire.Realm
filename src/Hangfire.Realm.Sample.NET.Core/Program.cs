f9bc8ca79188cca470b6fc1687d8fa914256b43a
Gregory Buchenberger <buchenberger@gmail.com>
2019-08-19 15:57:33 -0600

    setDto changes

--- src/Hangfire.Realm.Sample.NET.Core/Program.cs
+++ src/Hangfire.Realm.Sample.NET.Core/Program.cs
@@ -39,17 +39,17 @@ namespace Hangfire.Realm.Sample.NetCore
 
             using (new BackgroundJobServer(serverOptions))
             {
-                for (var i = 0; i < JobCount; i++)
-                {
-                    var jobNumber = i + 1;
-                    var jobId = BackgroundJob.Enqueue(() =>
-                    Console.WriteLine($"Fire-and-forget job {jobNumber}"));
-                    //Console.WriteLine($"Job {jobNumber} was given Id {jobId} and placed in queue");
-                }
+                //for (var i = 0; i < JobCount; i++)
+                //{
+                //    var jobNumber = i + 1;
+                //    var jobId = BackgroundJob.Enqueue(() =>
+                //    Console.WriteLine($"Fire-and-forget job {jobNumber}"));
+                //    Console.WriteLine($"Job {jobNumber} was given Id {jobId} and placed in queue");
+                //}
 
-                BackgroundJob.Schedule(() =>
-                Console.WriteLine("Scheduled job"),
-                TimeSpan.FromSeconds(60));
+                //BackgroundJob.Schedule(() =>
+                //Console.WriteLine("Scheduled job"),
+                //TimeSpan.FromSeconds(60));
 
                 RecurringJob.AddOrUpdate(Guid.NewGuid().ToString(), () => Console.Write("Recurring job"), Cron.Minutely);
 
